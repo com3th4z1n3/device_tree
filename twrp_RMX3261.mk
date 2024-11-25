5a687f847a4b8b86da4cb513dedf42b236f22887
com3th4z1n3 <exsenxxx@proton.me>
2024-11-25 18:36:43 +0700

    for PBrb

--- twrp_RMX3261.mk
+++ twrp_RMX3261.mk
@@ -2,7 +2,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, device/realme/RMX3261/device.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_DEVICE := RMX3261
 PRODUCT_NAME := twrp_RMX3261
