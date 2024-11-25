4f9676db9b9aff8570e5f4cca74782d68672fce2
com3th4z1n3 <exsenxxx@proton.me>
2024-11-25 22:12:53 +0700

    Update omni_RMX3261.mk

--- omni_RMX3261.mk
+++ omni_RMX3261.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := RMX3261
-PRODUCT_NAME := omni_RMX3261
+PRODUCT_NAME := pb_RMX3261
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3261
 PRODUCT_MANUFACTURER := realme
